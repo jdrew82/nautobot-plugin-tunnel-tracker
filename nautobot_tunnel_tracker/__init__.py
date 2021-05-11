cbec5ab5b11e20d60d51499214b2e111447d1b82
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2021-05-10 21:36:04 -0500

    first commit

---
+++ nautobot_tunnel_tracker/__init__.py
@@ -0,0 +1,24 @@
+"""Plugin declaration for nautobot_tunnel_tracker."""
+
+__version__ = "0.1.0"
+
+from nautobot.extras.plugins import PluginConfig
+
+
+class TunnelTrackerConfig(PluginConfig):
+    """Plugin configuration for the nautobot_tunnel_tracker plugin."""
+
+    name = "nautobot_tunnel_tracker"
+    verbose_name = "Nautobot Tunnel Tracker"
+    version = __version__
+    author = "Justin Drew"
+    description = "Nautobot Tunnel Tracker."
+    base_url = "tunnel-tracker"
+    required_settings = []
+    min_version = "1.0.0"
+    max_version = "1.9999"
+    default_settings = {}
+    caching_config = {}
+
+
+config = TunnelTrackerConfig  # pylint:disable=invalid-name
---
+++ nautobot_tunnel_tracker/urls.py
@@ -0,0 +1,4 @@
+"""Django urlpatterns declaration for nautobot_tunnel_tracker plugin."""
+# from django.urls import path
+
+urlpatterns = []
