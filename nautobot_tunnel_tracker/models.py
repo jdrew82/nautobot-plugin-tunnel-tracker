8e62771196502c27acb284013acf4b6966ba8ce9
Justin Drew <jdrew82@users.noreply.github.com>
2022-02-14 16:26:09 -0600

    refactor: :recycle: Make BaseTunnel abstract
    In order for other Tunnel objects to inherit from BaseTunnel it needs to be abstract. Removed unnecessary configuration parts for an abstract model.

--- nautobot_tunnel_tracker/models.py
+++ nautobot_tunnel_tracker/models.py
@@ -18,14 +18,6 @@ from .choices import (
 )
 
 
-@extras_features(
-    "custom_fields",
-    "custom_validators",
-    "export_templates",
-    "relationships",
-    "graphql",
-    "webhooks",
-)
 class BaseTunnel(PrimaryModel):
     """Base Tunnel model."""
 
@@ -33,56 +25,26 @@ class BaseTunnel(PrimaryModel):
     slug = models.SlugField(max_length=100, unique=True)
     description = models.CharField(max_length=200, blank=True)
     status = models.CharField(
-        max_length=30, choices=TunnelStatusChoices, default=TunnelStatusChoices.STATUS_PENDING_CONFIGURATION
+        max_length=30, choices=TunnelStatusChoices.CHOICES, default=TunnelStatusChoices.STATUS_PENDING_CONFIGURATION
     )
-    tunnel_type = models.CharField(max_length=30, choices=TunnelTypeChoices, default=TunnelTypeChoices.PPTP_TUNNEL)
-    src_device = models.OneToOneField(
-        to="dcim.Device", on_delete=models.CASCADE, help_text="Source Device", blank=False
+    tunnel_type = models.CharField(
+        max_length=30, choices=TunnelTypeChoices.CHOICES, default=TunnelTypeChoices.PPTP_TUNNEL
     )
-    src_interface = models.OneToOneField(
+    src_device = models.ForeignKey(
+        to="dcim.Device", on_delete=models.CASCADE, help_text="Source Device", blank=False, related_name="basetunnels"
+    )
+    src_interface = models.ForeignKey(
         to="dcim.Interface",
         on_delete=models.CASCADE,
         help_text="Source Interface",
         blank=False,
-        limit_choices_to=models.Q(app_label="dcim", model="interface", device_name=src_device),
+        related_name="basetunnels",
     )
-
-    csv_headers = [
-        "name",
-        "description",
-        "status",
-        "tunnel_type",
-        "src_device",
-        "src_interface",
-    ]
-
-    def to_csv(self):
-        """Indicates model fields to return as csv."""
-        return (
-            self.name,
-            self.description,
-            self.status,
-            self.tunnel_type,
-            self.src_device,
-            self.src_interface,
-        )
 
     class Meta:
         """Class to define what will be used to set order based on."""
 
-        ordering = ["name"]
-
-    def __str__(self):
-        """Class to define what identifies the BaseTunnel object."""
-        return self.name
-
-    def get_absolute_url(self):  # pylint: disable=no-self-use
-        """Absolute url for the Tunnel instance."""
-        return reverse("plugins:nautobot_tunnel_tracker:tunnels_list")
-
-    def save(self, *args, **kwargs):
-        """Save method for BaseTunnel class."""
-        return super().save(*args, **kwargs)
+        abstract = True
 
 
 @extras_features(
